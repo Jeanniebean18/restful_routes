c4ab3d74a97fe6ce8c934b3eec7309b942a9a207
Robert Plugge <robmplugge@hotmail.com>
2015-07-20 14:05:24 -0500

    Added more user controllers/views

--- controllers/users.rb
+++ controllers/users.rb
@@ -10,9 +10,40 @@ post "/users" do
   the_password = BCrypt::Password.create(params[:password])
   @user = User.create({name: params[:name], email: params[:email], password: the_password})
   if @user.valid?
-    redirect "users/:id"
+    redirect "users/@user.id"
   else
     @error = true
     erb :"users/new"
   end
+end
+
+get "/users/:id/edit" do
+  @user = User.find(params[:id])
+  erb :"users/edit"
+end
+
+put "/users/:id" do
+  @user = User.find(params[:id])
+  @user.name = params[:name]
+  @user.email = params[:email]
+  
+  @user.save
+  
+  redirect "users/@user.id"
+end
+
+get "users/delete" do
+  erb :"users/delete"
+end
+
+delete "/users" do
+  @user = User.find(params[:id])
+  @user.destroy
+  
+  redirect "/users"
+end
+
+get "users/:id" do
+  @user = User.find(params[:id])
+  erb :"user/show"
 end
